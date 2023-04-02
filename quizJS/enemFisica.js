434b8c75dceb18439bda2a34ff290861f0ce84b9
João <joaogabriel9512@gmail.com>
2023-04-02 09:18:38 -0300

    Implementação do quiz

---
+++ quizJS/enemFisica.js
@@ -0,0 +1,67 @@
+import EnemFisicaQuestions from "../quizQuestions/EnemFisicaQuestions.js";
+
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".quiz");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+let questions = EnemFisicaQuestions;
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+const resetQuiz = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+const handleAnswer = (e) => {
+  if (e.target.dataset.correct === "true") {
+    questionsCorrect++;
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+};
+
+const finish = () => {
+  textFinish.innerHTML = `Você acertou ${questionsCorrect} de ${questions.length} perguntas.`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
+};
+
+const loadQuestion = () => {
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", handleAnswer);
+  });
+};
+
+resetQuiz();
+btnRestart.addEventListener("click", resetQuiz);
---
+++ quizQuestions/EnemFisicaQuestions.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    question: "Quanto é 1 + 2?ksalksalksalksalksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawbndiuwbvdfuywbQuanto é 1 + 2?ksalksalksalksalksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawbndiuwbvdfuywbdiubnwdjkw djowndjwbdiuyvwduycvwdtQuanto é 1 + 2?ksalksalksalksalksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawbndiuwbvdfuywbdiubnwdjkw djowndjwbdiuyvwduycvwdtQuanto é 1 + 2?ksalksalksalksalksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawbndiuwbvdfuywbdiubnwdjkw djowndjwbdiuyvwduycvwdtQuanto é 1 + 2?ksalksalksalksalksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawbndiuwbvdfuywbdiubnwdjkw djowndjwbdiuyvwduycvwdtdiubnwdjkw djowndjwbdiuyvwduycvwdtuyb",
+    answers: [
+      { option: "3d3iowdhiu3edfuedfheiubnfQuanto é 1 + 2?ksalksalksalksalksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawbndiuwbvdfuywbdiubnwdjkw djowndjwbdiuyvwduycvwdtQuanto é 1 + 2?ksalksalksalksalksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawbndiuwbvdfuywbdiubnwdjkw djowndjwbdiuyvwduycvwdt", correct: true },
+      { option: "2", correct: false },
+      { option: "4", correct: false },
+      { option: "9", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 5 x 2?",
+    answers: [
+      { option: "9", correct: false },
+      { option: "10", correct: true },
+      { option: "11", correct: false },
+      { option: "91", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 10 x 8?",
+    answers: [
+      { option: "800", correct: false },
+      { option: "90", correct: false },
+      { option: "10", correct: false },
+      { option: "80", correct: true },
+    ],
+  },
+  {
+    question: "Quanto é 7 x 7?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "170", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "Quanto é 1 x 7?",
+    answers: [
+      { option: "7", correct: true },
+      { option: "170", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 1 x 1?",
+    answers: [
+      { option: "7", correct: false },
+      { option: "170", correct: false },
+      { option: "1", correct: true },
+      { option: "49", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 1 x 0?",
+    answers: [
+      { option: "0", correct: true },
+      { option: "170", correct: false },
+      { option: "1", correct: false },
+      { option: "49", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 1 x 0?",
+    answers: [
+      { option: "0", correct: true },
+      { option: "170", correct: false },
+      { option: "1", correct: false },
+      { option: "49", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 1 x 0?",
+    answers: [
+      { option: "0", correct: true },
+      { option: "170", correct: false },
+      { option: "1", correct: false },
+      { option: "49", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 1 x 0?",
+    answers: [
+      { option: "0", correct: true },
+      { option: "170", correct: false },
+      { option: "1", correct: false },
+      { option: "49", correct: false },
+    ],
+  },
+];
