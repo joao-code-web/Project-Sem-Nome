e5468a6c8599ea042622163778b8ad90a9b92b2b
João <joaogabriel9512@gmail.com>
2023-04-07 11:44:54 -0300

    mudança do header de todas as paginas

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,27 @@
-const buttonI = window.document.querySelector(".list-mobile i");
+const buttonI = document.querySelector(".list-mobile i");
 const desktop = document.querySelector("nav.desktop");
+const container = document.querySelector(".logo img");
 
 const menuFunction = () => {
   if (desktop.style.display == "none") {
-    desktop.style.display = "block"
-    desktop.style.opacity = "1"
-
+    desktop.style.display = "block";
+    desktop.style.opacity = "1";
   } else {
-    desktop.style.display = "none"
+    desktop.style.display = "none";
   }
 };
 
 buttonI.addEventListener("click", () => {
   menuFunction();
 });
+
+function animateImage() {
+  container.style.transform = "translateY(-60%)";
+}
+
+function resetAnimation() {
+  container.style.transform = "translateY(0)";
+}
+
+container.addEventListener("mouseenter", animateImage);
+container.addEventListener("mouseleave", resetAnimation);
