187226976939b66ed27b87cdf76718be096c23f2
João <joaogabriel9512@gmail.com>
2023-04-28 14:58:26 -0300

    novos icones

--- js/exerRapidos.js
+++ js/exerRapidos.js
@@ -3,10 +3,17 @@ const input = document.querySelector(".search-quiz input");
 
 const filterQuiz = (searchQuiz) => {
   searchQuiz = searchQuiz.toLowerCase();
+  let foundQuiz = false;
 
   exerSped.forEach((quiz) => {
-    console.log(quiz);
     let tittle = quiz.querySelector("h1").textContent.toLowerCase();
+    let shouldDisplay = tittle.includes(searchQuiz);
+
+    quiz.classList.toggle("hidden", !shouldDisplay);
+
+    if (shouldDisplay) {
+      foundQuiz = true;
+    }
 
     if (tittle.indexOf(searchQuiz) !== -1) {
       quiz.style.display = "block";
@@ -14,6 +21,12 @@ const filterQuiz = (searchQuiz) => {
       quiz.style.display = "none";
     }
   });
+
+  if (foundQuiz) {
+    console.log("encontrou");
+  } else {
+    console.log("não encontrou");
+  }
 };
 
 input.addEventListener("input", function () {
